593d00776ebe1d69c96eaf6104e254714d0ae837
Ayyah-Coded <gbolahanyahyah@gmail.com>
2026-07-08 15:45:28 +0100

    Implement routing and authentication with Clerk; Created sign-in , homepage, and dashboard pages

---
+++ src/auth/sign-in/index.jsx
@@ -0,0 +1,12 @@
+import { SignIn } from '@clerk/react';
+
+
+function SignInPage() {
+  return (
+    <div className='flex justify-center my-25 item-center'>
+      <SignIn/>
+    </div>
+  )
+}
+
+export default SignInPage;
--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,39 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import SignInPage from './auth/sign-in'
+import HomePage from './home'
+import Dashboard from './dashboard'
+import { ClerkProvider } from '@clerk/react'
+
+
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+const router = createBrowserRouter([
+  {
+    element:<App/>,
+    children: [
+      {
+        path:"/",
+        element:<HomePage/>
+      },
+      {
+        path:"/dashboard",
+        element:<Dashboard/>
+      }
+    ]
+  },
+  {
+    path:"/auth/sign-in",
+    element:<SignInPage/>
+  }
+])
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+      <RouterProvider router={router} />
+    </ClerkProvider>
   </StrictMode>,
 )
